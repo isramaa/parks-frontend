ff08c0f5659892c0e305f2e7dcc33f9e707fb9eb
Edgar Israel Martinez Ascencio <edisraelmartinez@gmail.com>
2025-09-21 22:36:39 -0600

    Resolviendo bug a la hora de pantalla de carga

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
   }
 
   return (
-    <div className="min-h-screen bg-green-50 flex flex-col">
+    <div className="min-h-screen min-w-screen bg-green-50 flex flex-col">
       {/* Header */}
       <header className="bg-green-200 text-white shadow-md">
         <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
